26cbeb706c113dc027e36fcfce39263eb2b4134c
Charlone Agius <charlone_91@hotmail.com>
2023-02-26 20:35:30 +0100

    fix meta tag on next js and font size on apple devices and background size on apple devices

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,12 +6,16 @@ import {UserProvider} from '@auth0/nextjs-auth0/client';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'react-toastify/dist/ReactToastify.css';
 import '../styles/globals.scss';
+import Head from "next/head";
 
 export default function App({Component, pageProps}: AppProps) {
   return (
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
         <UserProvider>
+          <Head>
+            <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, minimum-scale=1.0, user-scalable=no" />
+          </Head>
           <Component {...pageProps} />
         </UserProvider>
       </PersistGate>
