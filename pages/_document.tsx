7a6b108498e416a43ea78ad994477c13df7c71fe
Charlone Agius <charlone_91@hotmail.com>
2023-02-26 19:47:14 +0100

    test viewport meta

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -11,7 +11,7 @@ class MyDocument extends Document {
       <Html>
         <Head>
           <meta charSet="utf-8" />
-          <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, minimum-scale=1.0, user-scalable=no" />
+          <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, minimum-scale=1.0" />
           <link
             rel="preload"
             href="_next/static/media/FridayNightLights.0f3c2621.ttf"
